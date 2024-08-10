1cb4f26228512e91cbec0e6bb8cf88a33ca78306
hakyulee <dlgkrbek123@gmail.com>
2024-08-10 17:08:57 +0900

    프론트 라우팅 구조 구축

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-rout er-dom';
 import './App.css';
+import LoginPage from './pages/login';
+import RegisterPage from './pages/register';
+import DashboardPage from './pages/dashboard';
 
 function App() {
-  return <div className="App">jex</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/login">
+          <LoginPage />
+        </Route>
+        <Route exact path="/register">
+          <RegisterPage />
+        </Route>
+        <Route exact path="/dashboard">
+          <DashboardPage />
+        </Route>
+        <Route path="/">
+          <Redirect to="/dashboard" />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
