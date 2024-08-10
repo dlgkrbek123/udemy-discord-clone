eec4247dbf762a185abd784513ab1ebe93be1404
hakyulee <dlgkrbek123@gmail.com>
2024-08-10 16:56:17 +0900

    redux와 함께 프론트 프로젝트 세팅

---
+++ front/src/store/reducers/authReducer.js
@@ -0,0 +1,12 @@
+const initState = {
+  userDetails: null,
+};
+
+const reducer = (state = initState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
