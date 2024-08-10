9c107f88915033a7590ddd035e8ffe7f811a300e
hakyulee <dlgkrbek123@gmail.com>
2024-08-10 14:24:51 +0900

    회원가입 로직 구현 완료

--- backend/controllers/auth/postRegister.js
+++ backend/controllers/auth/postRegister.js
@@ -1,5 +1,39 @@
+const User = require('../../models/user');
+const bcrypt = require('bcryptjs');
+
 const postRegister = async (req, res) => {
-  res.send('register routes');
+  try {
+    const { username, mail, password } = req.body;
+
+    const userExists = await User.exists({ mail: mail.toLowerCase() });
+
+    if (userExists) {
+      res.status(409).send('Email already in use');
+    }
+
+    // 비밀번호 암호화
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    // document를 database에 저장
+    const user = await User.create({
+      username,
+      mail: mail.toLowerCase(),
+      password: encryptedPassword,
+    });
+
+    // jwt 토큰을 생성
+    const token = 'JWT Token';
+
+    return res.status(201).json({
+      userDetails: {
+        mail: user.mail,
+        user: user.username,
+        token,
+      },
+    });
+  } catch (error) {
+    return res.status(500).send('Error occured. Please Try Again ');
+  }
 };
 
 module.exports = postRegister;
